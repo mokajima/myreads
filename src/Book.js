acd085d2f3d6422f54ed6dd86ab73cf448d1a379
mokajima <mokajima.85z@gmail.com>
2018-08-08 22:57:32 +0900

    feat: Display a book even if it does not have a thumbnail

--- src/Book.js
+++ src/Book.js
@@ -7,7 +7,9 @@ class Book extends React.Component {
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail}` }}></div>
+          {book.imageLinks &&
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail}` }}></div>
+          }
           <div className="book-shelf-changer">
             <select value={book.shelf} onChange={(event) => this.props.moveBook(book, event.target.value)}>
               <option value="move" disabled>Move to...</option>
