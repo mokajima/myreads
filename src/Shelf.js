023e8642df01c1ce19445e7c3c7c995cd745112f
mokajima <mokajima.85z@gmail.com>
2018-08-08 14:38:36 +0900

    feat: Add Book components

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import Book from './Book'
 
 class Shelf extends React.Component {
   render() {
@@ -12,22 +13,7 @@ class Shelf extends React.Component {
           <ol className="books-grid">
             {books.map((book) => (
               <li>
-                <div className="book">
-                  <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail}` }}></div>
-                    <div className="book-shelf-changer">
-                      <select>
-                        <option value="move" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="book-title">{book.name}</div>
-                  <div className="book-authors">{book.authors.join(', ')}</div>
-                </div>
+                <Book book={book} />
               </li>
             ))}
           </ol>
